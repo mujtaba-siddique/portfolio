37a7e173c815491a96c6b0da7ba805ad279f0764
mohammad ghous mujtaba <mohammad.ghous.mujtabaa@gmail.com>
2024-11-04 02:40:32 +0530

    cg

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ import { useState } from "react";
 function App() {
   const [darkModeComp, setDarkModeComp] = useState(true);
 
-  // Function to toggle the dark mode state
+  
   const handleDarkModeToggle = () => {
     setDarkModeComp(!darkModeComp);
   };
 
   return (
     <div className={darkModeComp ? 'bg-gray-900 text-white' : 'bg-gray-200 text-black'}>
-      {/* Passing the dark mode state to Navbar */}
+      
       <Navbar darkModeComp={darkModeComp} />
-      {/* Passing dark mode state and toggle function to Home */}
+     
       <Home darkModeComp={darkModeComp} onDarkModeToggle={handleDarkModeToggle} />
     </div>
   );
