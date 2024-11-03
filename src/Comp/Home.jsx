ba6330ceb9c3cf298b021c826e2b5456916e248b
mohammad ghous mujtaba <mohammad.ghous.mujtabaa@gmail.com>
2024-11-04 02:38:31 +0530

    newchages

--- src/Comp/Home.jsx
+++ src/Comp/Home.jsx
@@ -1,38 +1,29 @@
 import Light from "../assets/light.png";
-import Pic from"../assets/Pic.jpg"
+import Pic from "../assets/Pic.jpg";
 import './style.css';
-import React, { useState } from 'react';
 
-
-const Home = () => {
-  const [darkMode, setDarkMode] = useState(true);
-
-  const DarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-
+const Home = ({ darkModeComp, onDarkModeToggle }) => {
   return (
     <div
-      className={` text-center transition-colors duration-300  h-screen ${
-        darkMode ? 'bg-gray-900 text-white' : 'bg-gray-200 text-black'
-      }`}>
-
-      <div
-        className="flex flex-col items-center mb-10 ml-4 cursor-pointer"
-        >
-        <img 
-          src={Light} 
-          className="h-24 duration-300 sm:ml-[35rem] "onClick={DarkMode}/>
-
+      className={`text-center transition-colors duration-300 h-screen ${
+        darkModeComp ? 'bg-gray-900 text-white' : 'bg-gray-200 text-black'
+      }`}
+    >
+      <div className="flex flex-col items-center mb-10 ml-4 cursor-pointer">
+        <img
+          src={Light}
+          className="h-24 duration-300 sm:ml-[35rem]"
+          onClick={onDarkModeToggle}
+          alt="Toggle Dark Mode"
+        />
         <p className="text-xs tracking-wide mt-1 sm:ml-[35rem] mb-16">
-          {darkMode ? 'Dark Mode' : 'Light Mode'}
+          {darkModeComp ? 'Dark Mode' : 'Light Mode'}
         </p>
       </div>
 
       <h1 className="text-4xl md:text-6xl font-bold mb-16">
-  I AM <span className="text-purple-500 typing-effect shadow-sm">Mohammad Ghous Mujtaba</span>
-</h1>
-
+        I AM <span className="text-purple-500 typing-effect shadow-sm">Mohammad Ghous Mujtaba</span>
+      </h1>
 
       <p className="max-w-md mx-auto mb-8 px-4">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
@@ -40,14 +31,14 @@ const Home = () => {
       </p>
 
       <button
-       className="px-6 py-2 border-2 rounded-full text-purple-500 border-purple-500 hover:bg-purple-500 hover:text-white transition-colors duration-300">
+        className="px-6 py-2 border-2 rounded-full text-purple-500 border-purple-500 hover:bg-purple-500 hover:text-white transition-colors duration-300"
+      >
         <a href={Pic} download="../assets/Pic.jpg">
           Download Resume
         </a>
-      </button>  
+      </button>
     </div>
   );
 };
 
 export default Home;
-
